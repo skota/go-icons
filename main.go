3343c1e733554f8cf97fd0408cf379e0c46df613
skota <sriramkota@yahoo.com>
2018-04-17 08:18:14 -0400

    recreating main.go...

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
+import (
+	"flag"
+	utils "lib/utilities"
+)
+
 func main() {
 	//create flag vars
+	var (
+		filename  = flag.String("image", "foo", "Image name")
+		operation = flag.String("operation", "icon", "operation name - create splashes or icons")
+		osname    = flag.String("os", "ios", "Os name - ios or android")
+	)
+	flag.parse()
+	params = utils.Config{Image: *filename, Operation: *operation, Os: *osname}
 
-	//flag parse
-
-	//
+	checkparams(params)
 }
 
-func checkparams() {
+func checkparams(params utils.Config) {
+	//check file exists and is valid
 
+	//launcj do tasks
 }
